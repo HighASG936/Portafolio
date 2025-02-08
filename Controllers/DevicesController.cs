6f3a6cc93aab114c4bca3c43a13213a086d24a05
Aurelio Siordia <lz_rock@hotmail.com>
2025-02-07 23:39:08 -0600

    Add project files.

---
+++ Controllers/DevicesController.cs
@@ -0,0 +1,23 @@
+﻿using CursoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+public class DevicesController(HttpClient httpClient) : Controller
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+    public async Task<IActionResult> Index()
+    {
+        string apiUrl = "https://devicesinventory-production.up.railway.app/api/Device";
+        List<Device> items = [];
+
+        HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+        if (response.IsSuccessStatusCode)
+        {
+            string json = await response.Content.ReadAsStringAsync();
+            items = JsonSerializer.Deserialize<List<Device>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        return View(items);
+    }
+}
---
+++ Models/Device.cs
@@ -0,0 +1,12 @@
+﻿namespace CursoMVC.Models
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        public required string Part_Number { get; set; }
+        public required string Description { get; set; }
+        public required int Quantity { get; set; }
+        public string? Supplier { get; set; }
+
+    }
+}
