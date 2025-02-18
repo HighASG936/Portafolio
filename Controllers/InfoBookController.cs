526c590d5893eb9dded9140ff43a7119dd534347
Aurelio Siordia <lz_rock@hotmail.com>
2025-02-18 00:10:01 -0600

    details pages

---
+++ Controllers/InfoBookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using Portafolio.Repository;
+
+namespace Portafolio.Controllers
+{
+    public class InfoBookController(IRepository<Book> Book) : Controller
+    {
+        private readonly IRepository<Book> _book = Book;
+
+        public async Task<IActionResult> Details(int id) 
+        {
+            var book = await _book.GetById(id);
+            if (book is null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+    }
+}
